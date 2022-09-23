ce886108e87c5341e3be8913766a03174712b3ed
Viktoryia Ryzhkova <viktiria87654321@gmail.com>
2022-09-23 13:35:50 +0400

    added simplebar and change currency

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,14 @@
 import Swiper from '../lib/swiper-bundle.esm.browser.min.js';
 
+// SimpleBar
+
+new SimpleBar(document.querySelector('.country__list'), {
+    classNames: {
+        scrollbar: 'country__scrollbar',
+        track: 'country__track',
+    }
+});
+
 // Slider
 
 new Swiper('.goods__block', {
@@ -62,7 +71,46 @@ formInput.forEach((input, i) => {
     })
 })
 
-// Modal
+// Currency
+
+const dataCurrency = {};
+
+const formatCurrency = (value, currency) => {
+    return new Intl.NumberFormat('EU', {
+        style: 'currency',
+        currency,
+        maximumFractionDigits: 2,
+    }).format(value)
+}
+
+const showPrice = (currency = 'USD') => {
+    const priceElems = document.querySelectorAll('[data-price]');
+
+    priceElems.forEach(elem => {
+        elem.textContent = formatCurrency(elem.dataset.price * dataCurrency[currency], currency);
+
+    })
+}
+
+const myHeaders = new Headers();
+myHeaders.append("apikey", "tpiHsLmMjqHli9nCz71Mdwo1p3VL3U19");
+
+const requestOptions = {
+  method: 'GET',
+  redirect: 'follow',
+  headers: myHeaders
+};
+
+fetch("https://api.apilayer.com/fixer/latest&base=USD", requestOptions)
+  .then(response => response.json())
+  .then(result => {
+    Object.assign(dataCurrency, result.rates);
+    showPrice();
+  })
+  .catch(error => console.log('error', error));
+
+
+// Choices
 
 const countryBtn = document.querySelector('.country__btn');
 const countryWrapper = document.querySelector('.country__wrapper');
@@ -74,6 +122,10 @@ countryBtn.addEventListener('click', () => {
 countryWrapper.addEventListener('click', ({target}) => {
     if(target.classList.contains('country__choice')) {
         countryWrapper.classList.remove('country__wrapper_open');
+        showPrice(target.dataset.currency)
     }
 })
+
+
+
 
